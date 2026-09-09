c3efd46d198cdf753d3ebe4998a29027e0a0e180
Tom_TL <quariko@gmail.com>
2026-09-09 15:00:34 +0100

    Update TBW_Assistant.user.js

--- TBW_Assistant.user.js
+++ TBW_Assistant.user.js
@@ -2,7 +2,7 @@
 // @name         TBW Assistant
 // @author       Tom Harris
 // @namespace    https://github.com/Tom-TL/credit_cube_scripts
-// @version      1.4
+// @version      1.5
 // @description  Show TBW denial reason, auto-deny some reasons, quick Review to CRP and quick Deny popup, with Copy reason button and auto-denied notice.
 // @match        http*://*/plm.net/*
 // @updateURL    https://raw.githubusercontent.com/Tom-TL/credit_cube_scripts/main/TBW_Assistant.user.js
@@ -17,7 +17,7 @@
   const href = window.location.href.toLowerCase();
 
   // Версия скрипта для попапа обновления
-  const SCRIPT_VERSION = '1.4';
+  const SCRIPT_VERSION = '1.5';
   const VERSION_KEY = 'tbwAssistant_version_seen';
 
   if (href.includes('customerdetails.aspx')) {
@@ -1342,7 +1342,7 @@ function getCustomerIdFromPage() {
     if (lower.includes('unacceptable bank')) {
       return 'UNACCEPTABLE_BANK';
     }
-    if (lower.includes('unacceptable state')) {
+    if (/unacc\w*\s+state/i.test(lower)) {
       return 'UNACCEPTABLE_STATE';
     }
     if (lower.includes('not interested') || lower.includes('not interest')) {
@@ -1385,7 +1385,7 @@ function getCustomerIdFromPage() {
         ACTIVE_LOAN_WITH_US:
           /(cust\s+has\s+an\s+active\s+loan\s+with\s+us|active\s+loan\s+with\s+us)/i,
         UNACCEPTABLE_BANK: /unacceptable\s+bank/i,
-        UNACCEPTABLE_STATE: /unacceptable\s+state/i
+        UNACCEPTABLE_STATE: /unacc\w*\s+state/i
       };
 
       const regex = regexMap[reasonCode];
